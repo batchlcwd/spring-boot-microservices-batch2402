0fe5020dc660974424f7c012322d484f8d490cd8
batchlcwd <batchlcwd@gmail.com>
2024-07-30 19:20:29 +0530

    Extra work: why constructor injection is better ?

--- spring-jpa-ecom/src/main/java/com/boot/jpa/ecom/controller/HomeController.java
+++ spring-jpa-ecom/src/main/java/com/boot/jpa/ecom/controller/HomeController.java
@@ -1,4 +1,28 @@
 package com.boot.jpa.ecom.controller;
 
+import com.boot.jpa.ecom.repositories.ProductRepository;
+import com.boot.jpa.ecom.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
+
+    //field  injection
+
+//    @Autowired
+    private final ProductService productService;
+
+
+    //constructor inject: recommended
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void test(){
+     productService.getProductByPrice(120);
+    }
+
+
 }
