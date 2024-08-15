daa7f1be3ec89afc56c2b691df568178dc603b4d
batchlcwd <batchlcwd@gmail.com>
2024-08-15 23:17:00 +0530

    15/08/2024 spring scurity started , formbased , basic, security iternals

--- start-learn-back/src/main/java/com/elearn/app/config/SecurityConfig.java
+++ start-learn-back/src/main/java/com/elearn/app/config/SecurityConfig.java
@@ -1,2 +1,66 @@
-package com.elearn.app.config;public class SecurityConfig {
+package com.elearn.app.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // customization
+
+
+        //routes/urls
+
+//      httpSecurity.authorizeHttpRequests(e->{
+//          e.requestMatchers("/api/v1/categories").permitAll()
+//                  .anyRequest()
+//                  .authenticated();
+//      });
+
+        httpSecurity.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(HttpMethod.GET, "/api/v1/categories").permitAll()
+                        .requestMatchers("/client-login", "/client-login-process").permitAll()
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/courses").permitAll()
+                    .anyRequest()
+                    .authenticated();
+
+        });
+
+
+        httpSecurity.formLogin(
+                form ->
+                {
+                    form.loginPage("/client-login");
+                    form.usernameParameter("username");
+                    form.passwordParameter("userpassword");
+                    form.loginProcessingUrl("/client-login-process");
+                    form.successForwardUrl("/success");
+//                    form.successHandler();
+//                    form.failureHandler();
+
+                }
+
+        );
+
+//        httpSecurity.httpBasic(Customizer.withDefaults());
+//        //
+//
+//        httpSecurity.cors()
+//
+
+
+        return httpSecurity.build();
+    }
+
+
 }
